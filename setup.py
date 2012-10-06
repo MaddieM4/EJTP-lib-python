c170c047c2052faa010ca06c762b92e1f1cb5214
Philip Horger <campadrenalin@gmail.com>
2012-10-06 15:59:42 -0700

    Closes #30 - Increment version number to 0.9.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
 	name = 'ejtp',
-	version = '0.9.0',
+	version = '0.9.1',
 	description = 'Encrypted JSON Transport Protocol library',
 	author = 'Philip Horger',
 	author_email = 'philip.horger@gmail.com',
