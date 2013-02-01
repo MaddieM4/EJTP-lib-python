1472864b66291a2e40878106f1998d26588aa8ac
Iuri de Silvio <iurisilvio@gmail.com>
2013-02-01 10:02:57 -0200

    config.test_filenames accept an env_var parameter

--- ejtp/config.py
+++ ejtp/config.py
@@ -1,12 +1,22 @@
 import os
 
-def test_filenames(filenames):
+def test_filenames(filenames, env_var=None):
     '''
     Return a list with only existent filenames.
 
+    If env_var exists in environment variables, it will replace
+    filenames already loaded.
+
     >>> test_filenames(['resources/examplecache.json'])
     ['resources/examplecache.json']
+    >>> os.environ['SOME_PATH'] = 'resources/examplecache.json'
+    >>> test_filenames(['client.py'], env_var='SOME_PATH')
+    ['resources/examplecache.json']
     '''
+    env_files = os.environ.get(env_var, None)
+    if env_files:
+        filenames = tuple(env_files.split(':'))
+
     result = []
     for filename in filenames:
         filename = os.path.expanduser(filename)
