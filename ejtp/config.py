417ca09c22eda5bc2f5a08cc8d319519fbca91ee
Iuri de Silvio <iurisilvio@gmail.com>
2013-02-01 09:57:51 -0200

    Add config.test_filenames(filenames)

--- ejtp/config.py
+++ ejtp/config.py
@@ -1,2 +1,18 @@
+import os
+
+def test_filenames(filenames):
+    '''
+    Return a list with only existent filenames.
+
+    >>> test_filenames(['resources/examplecache.json'])
+    ['resources/examplecache.json']
+    '''
+    result = []
+    for filename in filenames:
+        filename = os.path.expanduser(filename)
+        if os.path.exists(filename):
+            result.append(filename)
+    return result
+
 def configure_ejtpd(filenames):
     raise NotImplementedError()
