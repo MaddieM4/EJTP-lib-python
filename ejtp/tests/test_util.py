d69b97ea506d3a157d939214df0f4760e31964fa
Iuri de Silvio <iurisilvio@gmail.com>
2013-02-01 10:48:21 -0200

    Add tests to hasher.make6 and hasher.maken

--- ejtp/tests/test_util.py
+++ ejtp/tests/test_util.py
@@ -4,6 +4,13 @@ from ejtp.util import hasher
 
 class TestHasher(unittest.TestCase):
 
+    expected_sha1 = 'e9a47e5417686cf0ac5c8ad9ee90ba2c1d08cc14'
+
     def test_make(self):
-        expected = 'e9a47e5417686cf0ac5c8ad9ee90ba2c1d08cc14'
-        self.assertEquals(expected, hasher.make('Sample string').export())
+        self.assertEqual(self.expected_sha1, hasher.make('Sample string').export())
+
+    def test_make6(self):
+        self.assertEqual(self.expected_sha1[:6], hasher.make6('Sample string').export())
+
+    def test_maken(self):
+        self.assertEqual(self.expected_sha1[:3], hasher.maken('Sample string', 3).export())
