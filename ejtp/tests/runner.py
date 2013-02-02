39f7cdf3bacf8f51fc57076bbc9d6f7c99327e67
Iuri de Silvio <iurisilvio@gmail.com>
2013-02-02 20:07:58 -0200

    Tests runner is a module instead of a script

--- ejtp/tests/runner.py
+++ ejtp/tests/runner.py
@@ -1,9 +1,12 @@
 import os
 import unittest
 
-base_path = os.path.split(__file__)[0]
+def main():
+    base_path = os.path.split(__file__)[0]
+    loader = unittest.TestLoader()
+    tests = loader.discover(base_path)
+    test_runner = unittest.runner.TextTestRunner()
+    test_runner.run(tests)
 
-loader = unittest.TestLoader()
-tests = loader.discover(base_path)
-test_runner = unittest.runner.TextTestRunner()
-test_runner.run(tests)
+if __name__ == '__main__':
+    main()
--- install_and_test.sh
+++ install_and_test.sh
@@ -4,4 +4,4 @@ python setup.py build
 sudo rm -r `ls /usr/local/lib/python2.*/dist-packages/ejtp -d`
 sudo python setup.py install
 doctestall ejtp
-python ejtp/tests/runner.py
+python -m ejtp.tests.runner
