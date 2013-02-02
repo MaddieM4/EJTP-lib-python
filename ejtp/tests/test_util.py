091d786a333d73abb377511ebe6d5944714e5dd9
Iuri de Silvio <iurisilvio@gmail.com>
2013-02-02 20:01:49 -0200

    Add tests to hasher.strict

--- ejtp/tests/test_util.py
+++ ejtp/tests/test_util.py
@@ -1,10 +1,10 @@
 import unittest
 
-from ejtp.util.py2and3 import RawData
+from ejtp.util.py2and3 import RawData, String
 from ejtp.util import hasher
 
 
-class TestHasher(unittest.TestCase):
+class TestHasherMake(unittest.TestCase):
 
     text = 'Sample string'
     expected_hash = 'e9a47e5417686cf0ac5c8ad9ee90ba2c1d08cc14'
@@ -20,3 +20,37 @@ class TestHasher(unittest.TestCase):
 
     def test_maken(self):
         self._assert(self.expected_hash[:3], hasher.maken(self.text, 3))
+
+
+class TestHasherStrict(unittest.TestCase):
+
+    def _assert(self, expected, value):
+        value = hasher.strict(value)
+        self.assertEqual(RawData(expected), RawData(value))
+
+    def test_String_object(self):
+        self._assert('"test"', String('test'))
+
+    def test_RawData(self):
+        self._assert('"test"', RawData('test'))
+
+    def test_bool(self):
+        self._assert('false', False)
+
+    def test_none(self):
+        self._assert('null', None)
+
+    def test_string(self):
+        self._assert('"test"', 'test')
+
+    def test_int(self):
+        self._assert('1', 1)
+
+    def test_list(self):
+        self._assert('["test1","test2"]', ['test1', 'test2'])
+
+    def test_tuple(self):
+        self._assert('["test1","test2"]', ('test1', 'test2'))
+
+    def test_dict(self):
+        self._assert('{"test1":1,"test2":2}', {'test1': 1, 'test2': 2})
