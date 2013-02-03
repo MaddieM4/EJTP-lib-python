d58aef763c2d78c9ea66fef125e2bf06ed820766
Iuri de Silvio <iurisilvio@gmail.com>
2013-02-02 22:20:28 -0200

    Implement -f option in ejtp-crypto script

--- scripts/ejtp-crypto-test.sh
+++ scripts/ejtp-crypto-test.sh
@@ -3,4 +3,9 @@
 echo "banana" | ejtp-crypto encode id mitzi@lackadaisy.com > nanafile.txt
 cat nanafile.txt | ejtp-crypto decode id mitzi@lackadaisy.com
 
+echo "banana" > nanafile.txt
+ejtp-crypto encode id mitzi@lackadaisy.com -f nanafile.txt > nananafile.txt
+ejtp-crypto decode id mitzi@lackadaisy.com -f nananafile.txt
+
 rm nanafile.txt
+rm nananafile.txt
