751ce467308acc532d73fde719fb1029aaeeb0d9
Iuri de Silvio <iurisilvio@gmail.com>
2013-02-03 13:13:32 -0200

    Add 'ecc' to encryptor.make factory

--- ejtp/crypto/encryptor.py
+++ ejtp/crypto/encryptor.py
@@ -134,5 +134,8 @@ def make(data):
     elif t=="rsa":
         from . import rsa
         return rsa.RSA(*args)
+    elif t=="ecc":
+        from . import ecc
+        return ecc.ECC(*args)
     else:
         raise TypeError("Unsupported encryption type: %r"%data)
