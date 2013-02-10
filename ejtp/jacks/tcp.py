f9c2d9f30d4f7da83f0bc3e7bee0b5873f815c81
Iuri de Silvio <iurisilvio@gmail.com>
2013-02-10 14:23:38 -0200

    Remove socket.create_connection to support Python 2.5

--- ejtp/jacks/tcp.py
+++ ejtp/jacks/tcp.py
@@ -117,7 +117,8 @@ class TCPConnection(stream.Connection):
                 self.connection = socket.socket(family, socktype, proto)
                 self.connection.connect(advsockaddr)
             else:
-                self.connection = socket.create_connection(sockaddr)
+                self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.connection.connect(sockaddr)
         self.connection.settimeout(1)
         self.start()
 
