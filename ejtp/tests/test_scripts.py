7a11e292dbf399af8e48179b010cba88f033cb28
Iuri de Silvio <iurisilvio@gmail.com>
2013-02-11 12:39:50 -0200

    Add tests to ejtp-crypto (encode|decode) (with)

--- ejtp/tests/test_scripts.py
+++ ejtp/tests/test_scripts.py
@@ -139,8 +139,7 @@ class TestConsole(unittest.TestCase):
 class TestCrypto(unittest.TestCase):
 
     def _import(self):
-        file_ = os.path.abspath(os.path.join(root, 'scripts', 'ejtp-crypto'))
-        with open(file_, 'rb') as fp:
+        with open(os.path.join(root, 'scripts', 'ejtp-crypto'), 'rb') as fp:
             return imp.load_module('crypto', fp, 'ejtp-crypto', ('.py', 'rb', imp.PY_SOURCE))
 
     def setUp(self):
@@ -149,16 +148,27 @@ class TestCrypto(unittest.TestCase):
             self.crypto = crypto
         self.io = IOMock()
 
-    def test_encode_decode_id(self):
-        argv = ['ejtp-crypto', 'encode', 'id', 'mitzi@lackadaisy.com']
-        self.io.input.append('banana')
+
+    def _test_encode_decode(self, text, id_=None, with_=None):
+        if id_:
+            args = ['id', id_]
+        elif with_:
+            args = ['with', with_]
+
+        argv = ['ejtp-crypto', 'encode'] + args
+        self.io.input.append(text)
         with self.io:
             self.crypto.main(argv)
 
-        argv = ['ejtp-crypto', 'decode', 'id', 'mitzi@lackadaisy.com']
+        argv = ['ejtp-crypto', 'decode'] + args
         self.io.pipe()
         with self.io:
             self.crypto.main(argv)
 
-        self.assertEqual('banana', self.io.get_value())
+        self.assertEqual(text, self.io.get_value())
 
+    def test_encode_decode_id(self):
+        self._test_encode_decode(text='banana', id_='mitzi@lackadaisy.com')
+
+    def test_encode_decode_with(self):
+        self._test_encode_decode(text='banana', with_='["rsa","-----BEGIN RSA PRIVATE KEY-----\\nMIICXAIBAAKBgQDAZQNip0GPxFZsyxcgIgyvuPTHsruu66DBsESG5/Pfbcye3g4W\\nwfg+dBP3IfUnLB4QXGzK42BAd57fCBXOtalSOkFoze/C2q74gYFBMvIPbEfef8yQ\\n83uoNkYAFBVp6yNlT51IQ2mY19KpqoyxMZftxwdtImthE5UG1knZE64sIwIDAQAB\\nAoGAIGjjyRqj0LQiWvFbU+5odLGTipBxTWYkDnzDDnbEfj7g2WJOvUavqtWjB16R\\nDahA6ECpkwP6kuGTwb567fdsLkLApwwqAtpjcu96lJpbRC1nq1zZjwNB+ywssqfV\\nV3R2/rgIEE6hsWS1wBHufJeqBZtlkeUp/VEx/uopyuR/WgECQQDJOaFSutj1q1dt\\nNO23Q6w3Ie4uMQ59rWeRxXA5+KjDZCxrizzo/Bew5ZysJzHB2n8QQ15WJ7gTSjwJ\\nMQdl/7SJAkEA9MQG/6JivkhUNh45xMYqnMHuutyIeGE17QndSfknU+8CX9UBLjsL\\nw1QU+llJ3iYfMPEDaydn0HJ8+iinyyAISwJAe7Z2vEorwT5KTdXQoG92nZ66tKNs\\naVAG8NQWH04FU7tuo9/C3uq+Ff/UxvKB4NDYdcM1aHqa7SEir/P4vHjtIQJAFKc9\\n1/BB2MCNqoteYIZALj4HAOl+8nlxbXD5pTZK5UAzuRZmJRqCYZcEtiM2onIhC6Yq\\nna4Tink+pnUrw24OhQJBAIjujQS5qwOf2p5yOqU3UYsBv7PS8IitmYFARTlcYh1G\\nrmcIPHRtkxIwNuFxy3ZRRPEDGFa82id5QHUJT8sJbqY=\\n-----END RSA PRIVATE KEY-----"]')
