d0f8f25c23a9b20e39518eb46d8e8281859746b5
Iuri de Silvio <iurisilvio@gmail.com>
2013-02-11 14:04:46 -0200

    Make ECC tests based on installed dependency instead of on Python version

--- ejtp/tests/test_crypto.py
+++ ejtp/tests/test_crypto.py
@@ -16,20 +16,24 @@ along with the Python EJTP library.  If not, see
 <http://www.gnu.org/licenses/>.
 '''
 
-import sys
-from ejtp.util.compat import unittest, is_py3k
-
+from ejtp.util.compat import unittest
 from ejtp.crypto.ecc import ECC
 
-
-skipPy3k = unittest.skipIf(is_py3k, 'Python 3.x does not have PyECC dependency.')
-onlyPy3k = unittest.skipUnless(is_py3k, 'Python 2.x have PyECC dependency.')
+try:
+    import pyecc
+    has_ecc = True
+except ImportError:
+    has_ecc = False
 
 
 class TestECC(unittest.TestCase):
 
     private_key = '!!![t{l5N^uZd=Bg(P#N|PH#IN8I0,Jq/PvdVNi^PxR,(5~p-o[^hPE#40.<|'
     public_key = '#&M=6cSQ}m6C(hUz-7j@E=>oS#TL3F[F[a[q9S;RhMh+F#gP|Q6R}lhT_e7b'
+
+    def setUp(self):
+        if not has_ecc:
+            self.skipTest('PyECC is not installed.')
 
     def test_can_encrypt(self):
         ecc = ECC(self.public_key, self.private_key, 'p184')
@@ -52,11 +56,11 @@ class TestECC(unittest.TestCase):
         self.assertEqual('test', decrypted.export())
 
 
-class TestECCPy3K(unittest.TestCase):
+class TestECCNotInstalled(unittest.TestCase):
 
-    def test_should_fail(self):
-        self.assertRaises(TypeError, ECC, None, None, 'p184')
+    def setUp(self):
+        if has_ecc:
+            self.skipTest('PyECC is installed.')
 
-
-TestECC = skipPy3k(TestECC)
-TestECCPy3K = onlyPy3k(TestECCPy3K)
+    def test_ecc_should_raise(self):
+        self.assertRaisesRegexp(TypeError, 'PyECC is not installed', ECC, None, None, None)
