ac73dca2993e27ef2f33d51db350da81b4feec81
Moritz Sichert <moritz.sichert@googlemail.com>
2013-02-11 02:26:17 +0000

    Added gplv3 header to some files

---
+++ ejtp/vendor/__init__.py
@@ -0,0 +1,17 @@
+'''
+This file is part of the Python EJTP library.
+
+The Python EJTP library is free software: you can redistribute it 
+and/or modify it under the terms of the GNU Lesser Public License as
+published by the Free Software Foundation, either version 3 of the 
+License, or (at your option) any later version.
+
+the Python EJTP library is distributed in the hope that it will be 
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser Public License for more details.
+
+You should have received a copy of the GNU Lesser Public License
+along with the Python EJTP library.  If not, see 
+<http://www.gnu.org/licenses/>.
+'''
