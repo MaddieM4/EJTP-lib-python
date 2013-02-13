af63cc3ac7cf7419b8c44087a739040d6b242527
Iuri de Silvio <iurisilvio@gmail.com>
2013-02-13 17:17:37 -0200

    Run quiet setup.py commands

--- install_and_test.sh
+++ install_and_test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-python setup.py build
+python setup.py -q build
 sudo rm -r `ls /usr/local/lib/python2.*/dist-packages/ejtp -d`
-sudo python setup.py install
+sudo python setup.py -q install
 doctestall ejtp
 python ejtp/tests/runner.py
