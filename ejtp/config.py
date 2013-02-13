d89d51d1b48be45fd7cde4164ac6dcf625d661af
Iuri de Silvio <iurisilvio@gmail.com>
2013-02-13 12:35:39 -0200

    Tests to config module

--- ejtp/config.py
+++ ejtp/config.py
@@ -24,12 +24,6 @@ def test_filenames(filenames, env_var=None):
 
     If env_var exists in environment variables, it will replace
     filenames already loaded.
-
-    >>> test_filenames(['resources/examplecache.json'])
-    ['resources/examplecache.json']
-    >>> os.environ['SOME_PATH'] = 'resources/examplecache.json'
-    >>> test_filenames(['client.py'], env_var='SOME_PATH')
-    ['resources/examplecache.json']
     '''
     if env_var:
         env_files = os.environ.get(env_var, None)
@@ -46,12 +40,6 @@ def test_filenames(filenames, env_var=None):
 def configure_identity_cache(cache, filenames=['~/.ejtp/idents.json', '~/.ejtp/console/idents.json']):
     '''
     Configure cache loading filenames.
-    
-    >>> from ejtp.identity.cache import IdentityCache
-    >>> ic = IdentityCache()
-    >>> configure_identity_cache(ic, ['resources/examplecache.json'])
-    >>> ic.find_by_name('mitzi@lackadaisy.com').location # doctest: +ELLIPSIS
-    [...'local', None, ...'mitzi']
     '''
     for filename in test_filenames(filenames, 'EJTP_IDENTITY_CACHE_PATH'):
         cache.load_from(filename)
---
+++ ejtp/tests/test_config.py
@@ -0,0 +1,27 @@
+import os
+
+from ejtp.util.compat import unittest
+from ejtp import config
+
+class TestConfig(unittest.TestCase):
+
+    config_file = 'resources/examplecache.json'
+
+    def test_test_filenames(self):
+        self.assertEqual([self.config_file],
+            config.test_filenames([self.config_file, 'some/invalid/file.json']))
+
+    def test_test_filenames_env_var(self):
+        os.environ['SOME_PATH'] = self.config_file + ':another/invalid/file.json'
+        self.assertEqual([self.config_file],
+            config.test_filenames(['some/invalid/file.json'], env_var='SOME_PATH'))
+
+    def test_configure_identity_cache(self):
+        from ejtp.identity.cache import IdentityCache
+        cache = IdentityCache()
+        config.configure_identity_cache(cache, [self.config_file])
+        self.assertEqual(['local', None, 'mitzi'],
+            cache.find_by_name('mitzi@lackadaisy.com').location)
+
+    def test_configure_ejtpd(self):
+        self.assertRaises(NotImplementedError, config.configure_ejtpd, [])
