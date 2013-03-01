77bb80e1e1fd13b1c10a2055aba0c8cfb46f9e04
Iuri de Silvio <iurisilvio@gmail.com>
2013-03-01 20:38:50 -0300

    Using class decorators

--- ejtp/tests/test_crypto.py
+++ ejtp/tests/test_crypto.py
@@ -94,14 +94,11 @@ class TestRSA(unittest.TestCase):
         self.assertFalse(make(myrsa.public()).can_encrypt())
 
 
+@unittest.skipUnless(has_ecc, 'PyECC is not installed.')
 class TestECC(unittest.TestCase):
 
     private_key = '!!![t{l5N^uZd=Bg(P#N|PH#IN8I0,Jq/PvdVNi^PxR,(5~p-o[^hPE#40.<|'
     public_key = '#&M=6cSQ}m6C(hUz-7j@E=>oS#TL3F[F[a[q9S;RhMh+F#gP|Q6R}lhT_e7b'
-
-    def setUp(self):
-        if not has_ecc:
-            self.skipTest('PyECC is not installed.')
 
     def test_can_encrypt(self):
         ecc = ECC(self.public_key, self.private_key, 'p184')
@@ -123,11 +120,8 @@ class TestECC(unittest.TestCase):
         self.assertEqual('test', decrypted.export())
 
 
+@unittest.skipIf(has_ecc, 'PyECC is installed.')
 class TestECCNotInstalled(unittest.TestCase):
-
-    def setUp(self):
-        if has_ecc:
-            self.skipTest('PyECC is installed.')
 
     def test_ecc_should_raise(self):
         self.assertRaisesRegexp(TypeError, 'PyECC is not installed', ECC, None, None, None)
