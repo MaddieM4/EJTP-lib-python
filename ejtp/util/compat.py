385e4e71c4256dd88208a09ed5ef1346c7cfcd25
Iuri de Silvio <iurisilvio@gmail.com>
2013-03-01 23:27:09 -0300

    Remove ejtp.util.compat.json

--- ejtp/util/compat.py
+++ ejtp/util/compat.py
@@ -23,8 +23,6 @@ if (2, 7) <= sys.version_info[:2] < (3, 0) or sys.version_info >= (3, 2):
 else:
     import unittest2 as unittest
 
-import json
-
 # Same as sys.version_info.major, but supports pre-2.7
 is_py3k = sys.version_info[0] == 3
 
