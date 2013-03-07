b77a4df265de777e25c5354d776fa94dd48c087c
Philip Horger <campadrenalin@gmail.com>
2013-03-06 19:14:43 -0800

    Metadata release 1 (0.9.3m1)

--- setup.py
+++ setup.py
@@ -2,13 +2,34 @@
 
 from distutils.core import setup
 
+long_desc = '''
+Encrypted JSON Transport Protocol
+=================================
+
+EJTP is an overlay protocol that allows the pluggable use of underlying transports, such as UDP, TCP, HTTP, IRC, Email and carrier pigeon to provide a cryptographically secure network of unreliable message forwarding. You can think of it as a bit like a more general-purpose and security-minded successor to XMPP, using JSON rather than XML as its frame medium.
+
+On top of a simple frame format, EJTP boasts a consistent and simple format for describing encryption credentials, which is useful even without the rest of EJTP. The ejtp-crypto script makes it easy for other projects to take advantage of this pending a native port of ejtp.crypto to languages other than Python.
+
+The intention of EJTP is to make it trivial to establish secure and NAT-oblivious distributed services across a common network of message relays. Your system only has to worry about exchanging encryption credentials and establishing a connection with a relay host, helping to pave the way toward distributed apps that run entirely in HTML5 (pending a port of the project to JS). You can be serverless *and* smartphone-friendly.
+
+Optionally supports elliptic curve cryptography if the PyECC_ module is installed.
+
+For more technical and in-depth information, visit the `Github project <https://github.com/campadrenalin/EJTP-lib-python>`.
+
+.. _PyECC: https://pypi.python.org/pypi/PyECC
+'''
+
 setup(
 	name = 'ejtp',
-	version = '0.9.3',
+	version = '0.9.3m1',
 	description = 'Encrypted JSON Transport Protocol library',
+    long_description = long_desc,
 	author = 'Philip Horger',
 	author_email = 'philip.horger@gmail.com',
 	url = 'https://github.com/campadrenalin/EJTP-lib-python/',
+    requires = [
+        'Crypto',
+    ],
     classifiers = [
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
