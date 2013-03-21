85b7976446561d0883a1750823c7cadcfc4ad4a3
Philip Horger <campadrenalin@gmail.com>
2013-03-20 18:31:43 -0700

    Made install_and_test.sh friendly to benchmarking

--- install_and_test.sh
+++ install_and_test.sh
@@ -3,5 +3,4 @@
 python setup.py build
 sudo rm -r `ls /usr/local/lib/python*.*/dist-packages/ejtp -d`
 sudo python setup.py install
-doctestall ejtp
-python ejtp/tests/runner.py
+doctestall ejtp && python ejtp/tests/runner.py && sh benchmark.sh
