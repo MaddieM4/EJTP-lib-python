d4181b90d354f9c2883ea27af62a4cea9d37d3ad
Moritz Sichert <moritz.sichert@googlemail.com>
2013-06-09 02:40:01 +0200

    Added Address class in ejtp.address

--- ejtp/address.py
+++ ejtp/address.py
@@ -15,40 +15,48 @@ You should have received a copy of the GNU Lesser Public License
 along with the Python EJTP library.  If not, see 
 <http://www.gnu.org/licenses/>.
 '''
-from json import loads
+import collections
+import json
 
-from persei import String, StringDecorator
+from persei import String
 
 from ejtp.util.hasher import strict
 
-@StringDecorator()
-def str_address(address):
-    '''
-        Converts address to string, only if it isn't already
-        >>> str_address([0,9])
-        '[0,9]'
-        >>> str_address("[0,9]")
-        '[0,9]'
-    '''
-    if isinstance(address, String):
-        return address
-    else:
-        return strict(address)
 
-@StringDecorator()
-def py_address(address):
+class Address(collections.namedtuple('_Address', ('addrtype', 'addrdetails', 'callsign'))):
     '''
-        Converts address to non-string, only if it isn't already
-        >>> py_address([0,9])
-        [0, 9]
-        >>> py_address("[0,9]")
-        [0, 9]
+    Represents an Address in EJTP.
+    
+    There are 3 fields: addrtype, addrdetails and callsign.
+    addrtype is the jack type that will be used (e.g. 'udp' or 'tcp6')
+    addrdetails are additional protocol specific parameters
+    callsign is used to identify different clients on the same jack. They only
+    get used by Clients.
     '''
-    if isinstance(address, String):
-        return loads(address.export())
-    elif isinstance(address, list):
-        return address
-    elif isinstance(address, tuple):
-        return loads(strict(address).export())
-    else:
-        raise ValueError("Can not convert to py_address: %r" % address)
+    __slots__ = ()
+
+    def __new__(cls, addrtype, addrdetails, callsign=None):
+        return super(Address, cls).__new__(cls, addrtype, addrdetails, callsign)
+
+    @classmethod
+    def create(cls, addr_string):
+        '''
+        Create address from json string.
+        '''
+        try:
+            addr_string = String(addr_string).export()
+        except (ValueError, TypeError):
+            pass
+        try:
+            addr = json.loads(addr_string)
+        except (ValueError, TypeError):
+            raise ValueError("couldn't parse json string: %r" % addr_string)
+        if len(addr) < 2 or len(addr) > 3:
+            raise ValueError('invalid address format')
+        return cls(*addr)
+    
+    def export(self):
+        '''
+        Returns address as json string.
+        '''
+        return strict(self)
