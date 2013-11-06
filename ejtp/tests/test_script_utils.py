e00ed93fe5529929294382e13f44614985f382f6
Philip Horger <campadrenalin@gmail.com>
2013-11-05 23:55:09 -0800

    #143 Add test for get_identity

--- ejtp/tests/test_script_utils.py
+++ ejtp/tests/test_script_utils.py
@@ -18,6 +18,7 @@ along with the Python EJTP library.  If not, see
 
 from ejtp.util.compat import unittest
 from ejtp.tests.test_scripts import IOMock
+from ejtp.identity import Identity
 
 from ejtp.util.scripts import *
 
@@ -63,7 +64,6 @@ class TestScriptUtils(unittest.TestCase):
             self.io.extend(['notinlist','still not in list', 'thing3'])
             result = choose(items, singular, plural)
             self.assertEqual(result, 'thing3')
-        self.maxDiff = None
         list_options = [
             'The following things are available:',
             'thing1 : t1 Description',
@@ -80,3 +80,86 @@ class TestScriptUtils(unittest.TestCase):
             list_options +
             ['$ thing3']
         )
+
+    def test_get_identity(self):
+        '''
+        Tries to overlap minimally with more specific tests, except for
+        the encryptor retry mechanism, which is specific to get_identity
+        and must not kill the process during encryptor creation failure.
+        '''
+        with self.io:
+            self.io.extend([
+                'boomer@galactica.gov',
+                'y',
+                'local',
+                'athena',
+                'y',
+                'y',
+                'rsa',
+                '120',
+                'y',
+                'rotate',
+                '120',
+                'y',
+            ])
+            result = get_identity()
+            expected_ident = Identity(
+                'boomer@galactica.gov',
+                ['rotate', 120],
+                ['local', None, 'athena']
+            )
+            self.assertEqual(result, expected_ident)
+        lines = self.io.get_lines()
+        expected_beginning = [
+            'NOTE: If you intend to host this identity in the DJDNS',
+            'registry, the email domain needs to match your branch',
+            'regex to be accessible.',
+            'http://roaming-initiative.com/blog/blog/djdns-ident-registration.html',
+            'Your identity name, in email form:',
+            '$ boomer@galactica.gov',
+            "The name you chose: 'boomer@galactica.gov'",
+            'Is this correct? [y/n]',
+            '$ y',
+            'The following types are available:',
+            'local : Can only communicate within a single OS process',
+            'tcp : IPv6 address, accessed over TCP',
+            'tcp4 : IPv4 address, accessed over TCP',
+            'udp : IPv6 address, accessed over UDP',
+            'udp4 : IPv4 address, accessed over UDP',
+            'Which type do you want?',
+            '$ local',
+            'Your callsign:',
+            '$ athena',
+            'Is this correct? [y/n]',
+            '$ y',
+            'Your location is:',
+            "['local', None, 'athena']",
+            'Is this correct? [y/n]',
+            '$ y',
+            'Now we generate your encryptor.',
+            'The following types are available:',
+            'rotate : Only for trivial demos, not recommended!',
+            'rsa : RSA Public-Key encryption (recommended)',
+            'Which type do you want?',
+            '$ rsa',
+            'How many bits?',
+            '$ 120',
+            'Is this correct? [y/n]',
+            '$ y',
+            'Generating... if it takes awhile, wiggle your mouse.',
+        ]
+        # Intentionally ignore the env-specific Crypto traceback
+        expected_end = [
+            "AttributeError: 'NoneType' object has no attribute 'exportKey'",
+            'The following types are available:',
+            'rotate : Only for trivial demos, not recommended!',
+            'rsa : RSA Public-Key encryption (recommended)',
+            'Which type do you want?',
+            '$ rotate',
+            'How much to rotate?',
+            '$ 120',
+            'Is this correct? [y/n]',
+            '$ y',
+        ]
+        self.assertEqual(lines[:len(expected_beginning)], expected_beginning)
+        self.assertEqual(lines[-len(expected_end):], expected_end)
